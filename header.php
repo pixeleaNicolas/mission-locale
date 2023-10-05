a7d73a7ae0a64daf1cf160d42cc33989d42708ab
Nicolas <nicolas.gehin@pixelea.fr>
2023-10-05 11:31:56 +0200

    header

--- header.php
+++ header.php
@@ -26,19 +26,18 @@
 	<?php wp_body_open(); ?>
 	<div id="page" class="site">
 		<a class="skip-link screen-reader-text visually-hidden" href="#main-content"><?php esc_html_e('Aller au contenu', 'mission-locale'); ?></a>
-
 		<div class="hidden" hiddden>
 			<?php get_template_part( 'dist/img/sprite.svg' ); ?>
 		</div>
 		
-		<header id="masthead" class="header" role="banner">
-			<div class="container">
-				<div class="site-branding">
-					<?php the_custom_logo(); ?>
-				</div><!-- .site-branding -->
-
+		<header id="masthead" class="header-main" role="banner">
+			<div class="header-main__container">
+					<?php $custom_logo_id = get_theme_mod('custom_logo'); 
+					if ( isset($custom_logo_id) ) {
+						 echo wp_get_attachment_image(  $custom_logo_id, "large", false, array("class" => "header-main__logo") ); 
+					}
+					?>
 				<?php get_template_part( 'template-parts/navigation/primary' ); ?>
-
 		</header><!-- #masthead -->
 
 		<main id="main-content" role="main">
